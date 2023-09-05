d6d03ebff7e91a4aad114293fb149e27b2e0a72c
lombosmonika <lombos.monika@gmail.com>
2023-09-05 18:23:24 +0200

    15. send email via google smtp

--- notification/src/main/java/com/example/notification/service/SMTPEmailService.java
+++ notification/src/main/java/com/example/notification/service/SMTPEmailService.java
@@ -1,18 +1,26 @@
 package com.example.notification.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Profile(value = "smtp")
-@Service
+@Service@RequiredArgsConstructor
 public class SMTPEmailService implements EmailService {
 
+    private final JavaMailSender mailSender;
 
     @Override
     public void send(String to, String subject, String body) {
         log.info("Email sending process has started with typed: {} to: {}", subject, to);
-
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        this.mailSender.send(message);
     }
 }
